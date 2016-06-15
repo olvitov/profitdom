ac197acf191475244c256cc7f4b05b6506c62f50
olvit <olvitov@gmail.com>
2016-06-15 16:32:32 +0300

    Добавлены в классе db method execute, query, fetchAll

--- App/DB.php
+++ App/DB.php
@@ -4,9 +4,31 @@ namespace App;
 
 class DB
 {
+    public $dbh;
     public function __construct()
     {
-        $this->dbh = new \PDO('mysql:host=127.0.0.0.1 dbname=jurnal', 'olvit', '34vit');
+        $this->dbh = new \PDO('mysql:host=127.0.0.1; dbname=jurnal', 'olvit', '34vit');
     }
+    
+    public function execute($sql) {
+        
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        return $res;
+        
+    }
+    
+    public function query($sql, $class) {
+        
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        
+        if (false !== $res) {
+            
+            return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
+        }
+    return[];    
+    }
+    
 
 }
