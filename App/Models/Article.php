e5dffbc16efabb2c469d9fbfae511a35a070f7bf
olvit <olvitov@gmail.com>
2016-06-16 09:22:54 +0300

    запросы приведены к App\Models\..............

---
+++ App/Models/Article.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: olvit
+ * Date: 16.06.2016
+ * Time: 8:59
+ */
+
+namespace App\Models;
+
+
+class Article
+{
+    public $title;
+    public $date;
+    public $description;
+    public $text;
+
+}
--- index.php
+++ index.php
@@ -4,9 +4,16 @@
 
 $db = new \App\Db();
 
-$data = $db->query('SELECT * FROM articles');
+$data = $db->query(
+    'SELECT * FROM articles',
+    'App\Models\Article'
 
-$data1 = $db->query('SELECT * FROM lessons');
+);
+
+$data1 = $db->query(
+    'SELECT * FROM lessons',
+    'App\Models\lesson'
+);
 
 
 var_dump($data);
